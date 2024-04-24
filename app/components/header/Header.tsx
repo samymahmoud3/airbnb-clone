cf8cdd4fbfa0478d5e28fbc17442b2ae66353884
samymahmoud3 <samymahmoud2532001@gmail.com>
2024-04-24 07:30:56 +0200

    header

---
+++ app/components/header/Header.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import SearchBar from './SearchBar'
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <header className='sticky top-0 z-50 shadow-md bg-white py-5'>
+      <div className='container grid grid-cols-3'>
+        <Link href='/' className='relative flex items-center h-10 my-auto'>
+          <Image
+            src='/airbnb.svg'
+            alt='logo'
+            fill
+            className='object-contain object-left'
+          />
+        </Link>
+        <SearchBar />
+        <Navbar />
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ app/components/header/SearchBar.tsx
@@ -0,0 +1,18 @@
+import { SearchIcon } from '@heroicons/react/solid';
+
+const SearchBar = () => {
+  return (
+    <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
+      <input
+        type='text'
+        placeholder='Start your search'
+        className='text-sm text-gray-600 placeholder-gray-400 flex-grow pl-5 bg-transparent outline-none'
+      />
+      <SearchIcon
+        className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'
+      />
+    </div>
+  )
+}
+
+export default SearchBar
