73bc9ee96f1fdbc5f6fd779c7d9de256a5db5fb2
samymahmoud3 <samymahmoud2532001@gmail.com>
2024-04-26 16:35:16 +0300

    greatest section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Banner from "./components/Banner";
 import Explore from "./components/Explore";
+import GreatestOutdoors from "./components/GreatestOutdoors";
 import Live from "./components/Live";
 
 export default function Home() {
@@ -8,6 +9,12 @@ export default function Home() {
       <Banner />
       <Explore />
       <Live />
+      <GreatestOutdoors
+        img='https://images.unsplash.com/photo-1609688669309-fc15db557633?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        title='The Greatest Outdoors'
+        description='Wishlists curated by Airbnb'
+        linkText='Get Inspired'
+      />
     </main>
   );
 }
